9b87941ec56dcd71957cab3c2baa2aeffa595791
rezasdsju <rezaul2002104@gmail.com>
2026-09-11 15:13:35 +0600

    style: update of UI

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,10 +3,10 @@ import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Nav() {
   return (
-    <nav className="sticky top-0 z-50 bg-slate-700 flex sm:grid grid-cols-3 mx-auto py-3">
+    <nav className="sticky top-0 z-50 bg-white flex sm:grid grid-cols-3 mx-auto py-3 shadow-md">
         
-        <h1 className="flex items-center ml-3 font-bold text-2xl"><span className="text-[#157AD5] ">R</span><span className="text-[#F9FAFB]">K</span><span className="text-[#5559F7]">R</span></h1>
-        <ul className="hidden sm:flex items-center gap-5  text-white">
+        <h1 className="flex items-center ml-3 font-bold text-2xl"><span className="text-[#157AD5] ">R</span><span className="text-[#32042f]">K</span><span className="text-[#5559F7]">R</span></h1>
+        <ul className="hidden sm:flex items-center gap-5  text-black">
             <li>Home</li>
             <li>About</li>
             <li>Projects</li>
@@ -16,7 +16,7 @@ export default function Nav() {
 
         </ul>
         <div className="mx-auto flex items-center">
-            <button className=" hidden md:flex sm:border-2 border-[#5559F7] bg-[#1C2948] text-white font-bold px-4 py-2  items-center rounded-2xl gap-2">Download CV<FaDownload /></button>
+            <button className=" hidden md:flex sm:border-1 border-[#9d9dc7] btn btn-secondary text-white font-bold px-4 py-2  items-center rounded-2xl gap-2">Download CV<FaDownload /></button>
         </div>
         <span className="flex justify-end items-center text-white pr-2 sm:hidden"><GiHamburgerMenu /></span>
     </nav>
