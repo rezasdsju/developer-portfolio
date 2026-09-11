824200482a7208dd0914adf8943371fc65bc28c8
rezasdsju <rezaul2002104@gmail.com>
2026-09-11 19:04:03 +0600

    style: update banner UI

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,13 +1,13 @@
 import BannarImage from '../assets/me-0.jpeg'
 export default function Banner() {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 my-5 sm:my-9'>
-        <div className='space-y-2 mx-auto px-3 sm:px-10'>
+    <div className='grid grid-cols-1 md:grid-cols-2 my-5 md:my-9'>
+        <div className='space-y-2 mx-auto px-3 md:px-10'>
             <h2 className='font-semibold text-2xl'>Hello, I am</h2>
-            <h1 className='text-3xl sm:text-5xl font-bold text-blue-900 '>Rezaul Karim Rifat</h1>
+            <h1 className='text-3xl md:text-5xl font-bold text-blue-900 '>Rezaul Karim Rifat</h1>
             <h2 className='bg-linear-to-r from-orange-900 to-purple-600 bg-clip-text text-transparent text-2xl font-semibold'>Full Stack Web Developer</h2>
             <p>I build modern and responsive web applications using Python, Django,React and Related Technologies. I love turning ideas into real products</p>
-            <div className='flex items-center gap-5 sm:gap-8 py-4'>
+            <div className='flex items-center gap-5 md:gap-8 py-4'>
                 <button className="btn btn-active btn-primary">View My Projects</button>
                 <button className="btn btn-outline">Contact Me</button>
             </div>
