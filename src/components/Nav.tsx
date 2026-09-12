e2c06384b143f1e8129238a5ae7cb4c3468523c0
rezasdsju <rezaul2002104@gmail.com>
2026-09-12 08:37:43 +0600

    feat: add hamburger menu toggle functionality with mobile navigation items

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,18 +1,26 @@
 
+import { useState } from "react";
 import { FaDownload } from "react-icons/fa";
+import { FaXmark } from "react-icons/fa6";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Nav() {
+  const [isHamburgerClicked, setIsHamburgerClicked] = useState<boolean>(false)
+  const handleHamburgerClick = ()=>{
+    setIsHamburgerClicked(!isHamburgerClicked)
+  }
   return (
-    <nav className="sticky top-0 z-50 bg-white/95 backdrop-blur-md flex sm:grid grid-cols-3 mx-auto py-3 shadow-sm border-b border-slate-100">
+    <div className="sticky top-0 z-50 ">
 
-      <h1 className="flex items-center ml-3 font-extrabold text-2xl tracking-tight mx-auto text-center sm:pl-8">
+    <nav className="bg-white/95 backdrop-blur-md flex sm:grid grid-cols-3 mx-auto py-3 shadow-sm border-b border-slate-100">
+
+      <h1 className={`flex items-center ml-3 font-extrabold text-2xl tracking-tight mx-auto text-center sm:pl-8 `}>
         <span className="text-[#157AD5]">R</span>
         <span className="text-[#32042f]">K</span>
         <span className="text-[#5559F7]">R</span>
       </h1>
 
-      <ul className="hidden sm:flex items-center justify-center gap-5 text-slate-700 font-medium text-sm">
+      <ul className={`hidden sm:flex items-center justify-center gap-5 text-slate-700 font-medium text-sm `}>
         <li className="cursor-pointer hover:text-[#157AD5] transition-colors duration-200">
           Home
         </li>
@@ -40,10 +48,47 @@ export default function Nav() {
         </button>
       </div>
 
-      <span className="flex justify-end items-center text-slate-800 pr-3 sm:hidden cursor-pointer">
-        <GiHamburgerMenu className="text-xl" />
+      <span onClick={handleHamburgerClick} className="flex justify-end items-center text-slate-800 pr-3 sm:hidden cursor-pointer">
+        {!isHamburgerClicked?<GiHamburgerMenu className="text-xl" />:<FaXmark className="text-xl"/>}
+
       </span>
 
     </nav>
+    <div className={`  ${isHamburgerClicked?'flex flex-col  items-center bg-lime-100 py-2':'hidden'}`}>
+      <ul className="font-bold text-[#157AD5]">
+        <li className="cursor-pointer  hover:text-[#157AD5] transition-colors duration-200">
+          Home
+        </li>
+        <li onClick={()=>{
+          document.getElementById('about')?.scrollIntoView();
+          setIsHamburgerClicked(!isHamburgerClicked)
+          }} className="cursor-pointer hover:text-[#157AD5] transition-colors duration-200">
+          About
+        </li>
+        <li onClick={()=>{
+          document.getElementById('projects')?.scrollIntoView();
+          
+          setIsHamburgerClicked(!isHamburgerClicked)}} className="cursor-pointer hover:text-[#157AD5] transition-colors duration-200">
+          Projects
+        </li>
+        <li onClick={() => {
+          document.getElementById('skill')?.scrollIntoView();
+          setIsHamburgerClicked(!isHamburgerClicked)}} className="cursor-pointer hover:text-[#157AD5] transition-colors duration-200">
+          Skills
+        </li>
+        <li onClick={()=>{
+          document.getElementById('experience')?.scrollIntoView()
+          setIsHamburgerClicked(!isHamburgerClicked)}} className="cursor-pointer hover:text-[#157AD5] transition-colors duration-200">
+          Experience
+        </li>
+        <li onClick={()=>{
+          document.getElementById('contact')?.scrollIntoView()
+          setIsHamburgerClicked(!isHamburgerClicked)}} className="cursor-pointer hover:text-[#157AD5] transition-colors duration-200">
+          Contact
+        </li>
+      </ul>
+    </div>
+          </div>
+
   );
 }
